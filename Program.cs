80213cddf44a3ce47da2a033cdb8d09176100cbc
jgravelle <j@gravelle.us>
2024-06-14 14:53:29 -0500

    Added *.yaml support w/ '-y' switch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.IO;
+
 class Program
 {
     static void Main(string[] args)
@@ -13,9 +15,10 @@ class Program
         // Create a StreamWriter to write the markdown content to a file
         using (StreamWriter markdownFile = new StreamWriter(Path.Combine(currentDirectory, markdownFileName)))
         {
-            // Traverse directories and get all .py files
+            // Traverse directories and get all .py and .yaml files
             var pythonFiles = Directory.GetFiles(currentDirectory, "*.py", SearchOption.AllDirectories);
 
+            // Process Python files
             foreach (var file in pythonFiles)
             {
                 // Read the content of each Python file
@@ -31,10 +34,30 @@ class Program
                 markdownFile.WriteLine(fileContent);
                 markdownFile.WriteLine("```");
                 markdownFile.WriteLine();
+            }
+            Console.WriteLine(args[0]);
+            if (args[0] == "-y")
+            {
+                var yamlFiles = Directory.GetFiles(currentDirectory, "*.yaml", SearchOption.AllDirectories);
+                foreach (var file in yamlFiles)
+                {
+                    // Read the content of each YAML file
+                    string fileContent = File.ReadAllText(file);
 
+                    // Get the relative path to make it clear where the file is from
+                    string relativePath = Path.GetRelativePath(currentDirectory, file);
+
+                    // Write the file path as a header in markdown
+                    markdownFile.WriteLine($"# {relativePath}");
+                    markdownFile.WriteLine();
+                    markdownFile.WriteLine("```yaml");
+                    markdownFile.WriteLine(fileContent);
+                    markdownFile.WriteLine("```");
+                    markdownFile.WriteLine();
+                }
             }
         }
 
-        Console.WriteLine($"All Python files have been compiled into {markdownFileName}");
+        Console.WriteLine($"All files have been compiled into {markdownFileName}");
     }
 }
